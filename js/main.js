ea0418694a8040f49a4b266d174d3af1a565081d
AhmedSalah <ahmedmahdy5005@gmail.com>
2021-07-07 17:39:21 +0200

    New Updates

--- js/main.js
+++ js/main.js
@@ -3,4 +3,14 @@ $(document).ready(function () {
         $(this).toggleClass('opened');
         $(".main-menu").slideToggle(200);
     })
+    var sticky = $(".sticky-nav")
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 100) {
+            $(".sticky-nav").addClass("colored");
+        } else {
+            $(".sticky-nav").removeClass("colored");
+        }
+    })
+
+
 })
